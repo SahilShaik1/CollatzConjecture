7263fe9f9f6dd48c3283159bf64e99a38c074faf
SahilShaik1 <89698739+SahilShaik1@users.noreply.github.com>
2022-07-20 16:10:44 -0400

    Create Collatz.java

---
+++ Collatz.java
@@ -0,0 +1,50 @@
+//💀💀💀💀
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+public class skull {
+	public static void main(String[] args) {
+		BigInteger Number;
+		BigInteger Count;
+		BigInteger two;
+		boolean finish = false;
+		Number = BigInteger.ONE;
+		Count = BigInteger.ZERO;
+		two = BigInteger.valueOf(2);
+		ArrayList<BigInteger> arr = new ArrayList<>();
+		while (true) {
+			if (Number.mod(two).compareTo(BigInteger.ZERO) == 0) {
+				//if even
+				Number = Number.divide(two);
+				arr.add(Number);
+			}
+			else {
+				//if odd
+				Number = Number.multiply(BigInteger.valueOf(3));
+				Number = Number.add(BigInteger.valueOf(1));
+				arr.add(Number);
+			}
+			if (Number.compareTo(BigInteger.valueOf(4)) == 0) {
+				//if normal
+				Count = Count.add(BigInteger.ONE);
+				System.out.println("Count is " + Count);
+				arr.clear();
+			}
+			//duplicate detection
+			Set<BigInteger> ar = new HashSet<BigInteger>();
+			for (BigInteger num : arr) {
+				if (ar.add(num) == false) {
+					System.out.println("Found");
+					System.out.println("Final repeating num is " + Number);
+					System.out.println("Count is " + Count);
+					finish = true;
+					break;
+				}
+			}
+			if (finish == true) {
+				break;
+			}
+		}
+	}
+}
